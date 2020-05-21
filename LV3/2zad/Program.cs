2aa1da78c3862c4cb3b0289af46f89a2b05614f3
Ivan9666 <65722063+Ivan9666@users.noreply.github.com>
2020-05-21 18:51:26 +0200

    Add files via upload

---
+++ LV3/2zad/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV3_2zad
+{
+    class Program
+    {
+        static void Main()
+        {
+            MatrixGenerator mg = MatrixGenerator.GetInstance();
+            double[][] matrix = mg.GenerateMatrix(5, 6);
+            foreach (double[] row in matrix)
+            {
+                foreach (double elem in row)
+                {
+                    System.Console.Write(elem + " ");
+                }
+                System.Console.WriteLine();
+            }
+
+        }
+    }
+}
