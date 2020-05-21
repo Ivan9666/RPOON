83eb838d91bd7b1bb632459b20c8d00996f20568
Ivan9666 <65722063+Ivan9666@users.noreply.github.com>
2020-05-21 19:14:40 +0200

    Add files via upload

---
+++ LV4/1zad/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV4
+{
+    class Program
+    {
+        static void Main()
+        { 
+        }
+        static void OutputSeparationLine()
+        {
+            System.Console.WriteLine("----------------------");
+        }
+    }
+}
