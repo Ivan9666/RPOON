533453de5d10ff095407ed05c8b9d6d37d4bf90b
Ivan9666 <65722063+Ivan9666@users.noreply.github.com>
2020-05-21 19:35:17 +0200

    Add files via upload

---
+++ LV4/2zad/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dataset data = new Dataset(System.AppDomain.CurrentDomain.BaseDirectory + "dataset.csv");
+            Analyzer3rdParty analyzer = new Analyzer3rdParty();
+            Adapter adapter = new Adapter(analyzer);
+
+            double[] rowAverage = adapter.CalculateAveragePerRow(data);
+            double[] columnAverage = adapter.CalculateAveragePerColumn(data);
+
+            System.Console.WriteLine("Column Averages:");
+            foreach (double n in columnAverage)
+            {
+                System.Console.Write(n + " ");
+            }
+
+            System.Console.WriteLine("\n----------------------");
+
+            System.Console.WriteLine("Row Averages:");
+            foreach (double n in rowAverage)
+            {
+                System.Console.WriteLine(n);
+            }
+        }
+    }
+}
