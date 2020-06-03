560518fbeba18474f442ad89afd18764c77108cb
Ivan9666 <65722063+Ivan9666@users.noreply.github.com>
2020-06-03 19:07:16 +0200

    Add files via upload

---
+++ LV6/drugi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("Keyboard", 22.20);
+            Product product2 = new Product("USB", 35.25);
+            Product product3 = new Product("Mouse", 35.25);
+
+            Box box = new Box();
+
+            box.AddProduct(product1);
+            box.AddProduct(product2);
+            box.AddProduct(product3);
+            box.RemoveProduct(product3);
+
+            IAbstractIterator boxIterator = box.GetIterator();
+
+            while (!boxIterator.IsDone)
+            {
+                Console.WriteLine(boxIterator.Current);
+                boxIterator.Next();
+            }
+        }
+        static void OutputSeparationLine()
+        {
+            System.Console.WriteLine("----------------------");
+        }
+    }
+}
---
+++ LV6/prvi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Note note1 = new Note("Cars", "BMW, OPEL, VW, Porsche");
+            Note note2 = new Note("Bikes", "Yammaha, Honda, KTM, Aprilia");
+            Note note3 = new Note("Tyres", "Tigar, Fulda, Sava,Dunlop");
+            Notebook notebook = new Notebook();
+
+            notebook.AddNote(note1);
+            notebook.AddNote(note2);
+            notebook.AddNote(note3);
+            notebook.RemoveNote(note2);
+
+            IAbstractIterator notebookIterator = notebook.GetIterator();
+
+            while (!notebookIterator.IsDone)
+            {
+                notebookIterator.Current.Show();
+                notebookIterator.Next();
+            }
+        }
+        static void OutputSeparationLine()
+        {
+            System.Console.WriteLine("--------------------");
+        }
+    }
+}
