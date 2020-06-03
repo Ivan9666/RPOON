3128e267b32920e21da3c050c7678f1dfbf88ecf
Ivan9666 <65722063+Ivan9666@users.noreply.github.com>
2020-06-03 18:12:53 +0200

    Add files via upload

---
+++ LV5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lv5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product inception = new Product("Leonardo Di Caprio", 10, 3);
+            Box movieBox = new Box("MovieBox");
+
+            movieBox.Add(inception);
+            Console.WriteLine(movieBox.Description());
+            Console.WriteLine(movieBox.Weight);
+
+            ShippingService service = new ShippingService(0.3);    
+            Console.WriteLine(service.shipping(movieBox.Weight));
+
+        }
+    }
+}
---
+++ LV5/ShippingService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lv5
+{
+    class ShippingService
+    {
+        private double priceForMass;
+
+        public ShippingService(double priceforMass)
+        {
+            this.priceForMass = priceforMass;
+        }
+
+        public double PriceForMass { get { return this.priceForMass; } }
+
+        internal double shipping(double weight)
+        {
+            return this.priceForMass * weight;
+        }
+
+    }
+}
